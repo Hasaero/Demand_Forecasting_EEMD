63b0691f77673c0a2d798f0b1e868708a3b11700
Hasaero <7infoheonou@gmail.com>
2023-05-07 16:48:47 +0900

    fit to augmented data, add r2 score

--- PyEMD/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-__version__ = "1.0.0"
-logger = logging.getLogger('pyemd')
-
-from PyEMD.EMD import EMD
-from PyEMD.EEMD import EEMD
-from PyEMD.CEEMDAN import CEEMDAN
-from PyEMD.visualisation import Visualisation
